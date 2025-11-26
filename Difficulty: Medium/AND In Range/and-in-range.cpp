4b2dfda4a580e860c5b001ac543a91c5af3ebadb
Chandan Behera <117701013+cbcpd@users.noreply.github.com>
2025-11-26 07:10:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/AND In Range/and-in-range.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    int andInRange(int l, int r) {
+        // code here
+       int shift = 0;
+        
+        while (l < r) {
+            l >>= 1;
+            r >>= 1;
+            shift++;
+        }
+        
+        return l << shift;
+    }
+};
