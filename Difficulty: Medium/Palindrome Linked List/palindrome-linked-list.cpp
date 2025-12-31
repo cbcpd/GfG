723e2fed6d17f53051773af11a125937faa4b1ad
Chandan Behera <117701013+cbcpd@users.noreply.github.com>
2025-12-31 08:54:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.cpp
@@ -0,0 +1,39 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+       data = x;
+       next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    
+    bool isPalindrome(Node *head) {
+        // Your code here
+        Node *slow=head,*fast=head->next;
+        while(fast and fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        Node *curr=slow->next,*prev=NULL,*nxt=NULL;
+        slow->next=NULL;
+        while(curr){
+            nxt=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+        }
+        while(prev){
+            if(prev->data!=head->data)return false;
+            prev=prev->next;
+            head=head->next;
+        }
+        return true;
+    }
+};
