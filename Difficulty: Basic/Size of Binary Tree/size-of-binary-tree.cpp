cdee9e848297a33744f195bc064d5902b71168e7
Chandan Behera <117701013+cbcpd@users.noreply.github.com>
2026-05-06 07:10:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of Binary Tree/size-of-binary-tree.cpp
@@ -0,0 +1,20 @@
+/*
+Definition for Node
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    int getSize(Node* root) {
+        if(!root) return 0;
+        return 1 + getSize(root->left) + getSize(root->right);
+    }
+};
