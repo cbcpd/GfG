5cd8bfdecf10366312beb3694095d65ca9583a99
Chandan Behera <117701013+cbcpd@users.noreply.github.com>
2026-05-16 06:46:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Not a subset sum/not-a-subset-sum.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    long long findSmallest(vector<int> &arr) {
+        sort(arr.begin(), arr.end());
+
+        long long res = 1;
+
+        for (int num : arr) {
+            if (num > res)
+                break;
+
+            res += num;
+        }
+
+        return res;
+    }
+};
