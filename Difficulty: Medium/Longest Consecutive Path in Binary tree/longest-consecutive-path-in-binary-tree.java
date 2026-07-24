c7e8b09eafb1620edc63911bb04030cfb8193598
Chandan Behera <117701013+cbcpd@users.noreply.github.com>
2026-07-24 07:13:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Path in Binary tree/longest-consecutive-path-in-binary-tree.java
@@ -0,0 +1,39 @@
+/* Structure of Binary Tree Node
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+}*/
+class Solution {
+    int ans = 1;
+    
+    public int longestConsecutive(Node root) {
+        if(root == null) return -1;
+        
+        solve(root, root.data-1, 0);
+        
+        return ans == 1 ? -1 : ans;
+    }
+    
+    private void solve(Node node, int parentVal, int len){
+        if(node == null){
+            return;
+        }
+        if(node.data == parentVal + 1){
+            len++;
+        }else{
+            len = 1;
+        }
+        ans = Math.max(ans, len);
+        
+        solve(node.left, node.data, len);
+        solve(node.right, node.data, len);
+    }
+}
